1ffb9c7cdf4ea8e55ca877c3f17f7cdfd0eae040
KalXD-g <kalyan3x90@gmail.com>
2025-01-04 21:14:31 +0530

    added wavelength functoinality

--- snell/main.py
+++ snell/main.py
@@ -2,7 +2,36 @@ import math
 import cv2 as cv
 import numpy as np
 
-screen = np.zeros((512, 512, 3), dtype=np.uint8) # height width channels 
+screen = np.zeros((512, 512, 3), dtype=np.uint8) # height width channels
+
+color_dictionary = {
+    1: (0, 0, 255),    # Red
+    2: (0, 155, 255),  # Orange
+    3: (0, 255, 255),  # Yellow
+    4: (0, 255, 0),    # Green
+    5: (255, 0, 0),    # Blue
+    6: (255, 0, 255)   # Purple
+}
+
+print('''
+1->Red
+2->Orange
+3->Yellow
+4->Green
+5->Blue
+6->Purple
+''')
+
+light_color=int(input("Choose the color of light::"))
+
+wavelength_dictionary={
+    1:620, # Red
+    2:590, # Orange
+    3:570, # Yellow
+    4:495, # Green
+    5:450, # Blue
+    6:380  # Purple
+}
 
 
 #TODO Uncomment below line later (restric taking 90 as input)
@@ -29,8 +58,8 @@ calculated_point_reflected_ray=(reference_point[0]-calculate_y(), reference_poin
 
 cv.line(screen, (256,0),(256,512),(105,105,105),1) # normal line
 cv.line(screen, (0,350),(512,350),(255,255,255),1) # media separator
-cv.line(screen, centre_point, calculated_point, (0,255,0),1)
-cv.line(screen, centre_point, calculated_point_reflected_ray, (0,255,0),1)
+cv.line(screen, centre_point, calculated_point, color_dictionary[light_color],1)
+cv.line(screen, centre_point, calculated_point_reflected_ray, color_dictionary[light_color],1)
 
 if screen is None:
     raise ValueError("Image not found")
