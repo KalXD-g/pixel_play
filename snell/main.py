924931ff1697e3932cae67cd1bae776ef0c2b11d
KalXD-g <kalyan3x90@gmail.com>
2025-01-04 21:20:14 +0530

    fixed minor typos

--- snell/main.py
+++ snell/main.py
@@ -2,7 +2,7 @@ import math
 import cv2 as cv
 import numpy as np
 
-screen = np.zeros((512, 512, 3), dtype=np.uint8) # height width channels
+screen = np.zeros((512, 512, 3), dtype=np.uint8)  # height width channels
 
 color_dictionary = {
     1: (0, 0, 255),    # Red
@@ -22,44 +22,43 @@ print('''
 6->Purple
 ''')
 
-light_color=int(input("Choose the color of light::"))
+light_color = int(input("Choose the color of light::"))
 
-wavelength_dictionary={
-    1:620, # Red
-    2:590, # Orange
-    3:570, # Yellow
-    4:495, # Green
-    5:450, # Blue
-    6:380  # Purple
+wavelength_dictionary = {
+    1: 620,  # Red
+    2: 590,  # Orange
+    3: 570,  # Yellow
+    4: 495,  # Green
+    5: 450,  # Blue
+    6: 380   # Purple
 }
 
+#TODO Uncomment below line later (restrict taking 90 as input)
+# incident_angle_deg = int(input("Incident angle (degrees)::"))
 
-#TODO Uncomment below line later (restric taking 90 as input)
-# incident_angle_deg=int(input("Incident angle (degrees)::"))
-
-incident_angle_deg=30
-incident_angle_rad=math.radians(incident_angle_deg)
+incident_angle_deg = 30
+incident_angle_rad = math.radians(incident_angle_deg)
 
 # Centre Point
-centre_point=(256,350)
+centre_point = (256, 350)
 
-# Reference Point to calculate the other coordiante
-reference_point=(256,0)
+# Reference Point to calculate the other coordinate
+reference_point = (256, 0)
 
 def calculate_y():
-    length=(reference_point[1]-centre_point[1])*math.tan(incident_angle_rad)
+    length = (reference_point[1] - centre_point[1]) * math.tan(incident_angle_rad)
     return int(length)
 
-#! The original idea was to add 1 to y-coordinate of calculated_point because the resultant line will be off by some pixels which is the resultant of flooring the length calculated from calculate_y() function. Different angles require different amount of compensation so I decided to ignore it completely
-calculated_point=(reference_point[0]+calculate_y(), reference_point[1])
+#! Originally planned to adjust the y-coordinate by 1 due to pixel rounding errors, but decided to ignore it for simplicity.
+calculated_point = (reference_point[0] + calculate_y(), reference_point[1])
 
-# Calculating end point for reflected ray
-calculated_point_reflected_ray=(reference_point[0]-calculate_y(), reference_point[1])
+# Calculating the endpoint for the reflected ray
+calculated_point_reflected_ray = (reference_point[0] - calculate_y(), reference_point[1])
 
-cv.line(screen, (256,0),(256,512),(105,105,105),1) # normal line
-cv.line(screen, (0,350),(512,350),(255,255,255),1) # media separator
-cv.line(screen, centre_point, calculated_point, color_dictionary[light_color],1)
-cv.line(screen, centre_point, calculated_point_reflected_ray, color_dictionary[light_color],1)
+cv.line(screen, (256, 0), (256, 512), (105, 105, 105), 1)  # normal line
+cv.line(screen, (0, 350), (512, 350), (255, 255, 255), 1)  # media separator
+cv.line(screen, centre_point, calculated_point, color_dictionary[light_color], 1)
+cv.line(screen, centre_point, calculated_point_reflected_ray, color_dictionary[light_color], 1)
 
 if screen is None:
     raise ValueError("Image not found")
@@ -67,7 +66,7 @@ if screen is None:
 cv.imshow('Snell\'s Law Visualizer', screen)
 
 while True:
-    if cv.waitKey(1) & 0xFF==32:
+    if cv.waitKey(1) & 0xFF == 32:
         break
 
 cv.destroyAllWindows()
