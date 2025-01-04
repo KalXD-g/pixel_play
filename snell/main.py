00dc11569599488340f93e2245a9d88ff472cd5e
KalXD-g <kalyan3x90@gmail.com>
2025-01-04 23:03:56 +0530

    ignore commit

--- snell/main.py
+++ snell/main.py
@@ -36,29 +36,44 @@ wavelength_dictionary = {
 #TODO Uncomment below line later (restrict taking 90 as input)
 # incident_angle_deg = int(input("Incident angle (degrees)::"))
 
-incident_angle_deg = 30
+incident_angle_deg = 45
 incident_angle_rad = math.radians(incident_angle_deg)
+
+# Refractive index of Upper medium
+upper_refractive_index=float(input("Refractive Index of Upper medium::"))
+
+# Refractive index of Upper medium
+lower_refractive_index=float(input("Refractive Index of Lower medium::"))
+
+#* Calculate Angle of Refraction (Result is in Radians)
+refraction_angle=math.asin((upper_refractive_index/lower_refractive_index)*(math.sin(incident_angle_rad)))
 
 # Centre Point
 centre_point = (256, 350)
 
-# Reference Point to calculate the other coordinate
-reference_point = (256, 0)
+# Reference Point to calculate the other coordinate for incident ray
+upper_reference_point = (256, 0)
 
-def calculate_y():
-    length = (reference_point[1] - centre_point[1]) * math.tan(incident_angle_rad)
+# Reference Point to calculate the other coordinate for refracted ray
+lower_reference_point = (256,512)
+
+def calculate_y(angle):
+    length = (upper_reference_point[1] - centre_point[1]) * math.tan(angle)
     return int(length)
 
 #! Originally planned to adjust the y-coordinate by 1 due to pixel rounding errors, but decided to ignore it for simplicity.
-calculated_point = (reference_point[0] + calculate_y(), reference_point[1])
+calculated_point = (upper_reference_point[0] + calculate_y(incident_angle_rad), upper_reference_point[1])
+
+calculated_point_refracted_ray=(lower_reference_point[0]+calculate_y(refraction_angle), lower_reference_point[1])
 
 # Calculating the endpoint for the reflected ray
-calculated_point_reflected_ray = (reference_point[0] - calculate_y(), reference_point[1])
+calculated_point_reflected_ray = (upper_reference_point[0] - calculate_y(incident_angle_rad), upper_reference_point[1])
 
 cv.line(screen, (256, 0), (256, 512), (105, 105, 105), 1)  # normal line
 cv.line(screen, (0, 350), (512, 350), (255, 255, 255), 1)  # media separator
 cv.line(screen, centre_point, calculated_point, color_dictionary[light_color], 1)
 cv.line(screen, centre_point, calculated_point_reflected_ray, color_dictionary[light_color], 1)
+cv.line(screen, centre_point, calculated_point_refracted_ray, color_dictionary[light_color], 1)
 
 if screen is None:
     raise ValueError("Image not found")
